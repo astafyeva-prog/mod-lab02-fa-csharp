7973843fbf2e6f0eaf9913248c9f54db01dce23c
astafyeva-prog <astafeva0404@gmail.com>
2026-05-18 00:33:39 +0300

    Update UnitTest1.cs

--- fa.Tests/UnitTest1.cs
+++ fa.Tests/UnitTest1.cs
@@ -7,7 +7,7 @@ namespace fa.Tests
     [TestClass]
     public class FA1Tests
     {
-        private FA1 fa1;
+        private FA1 fa1 = null!;
         
         [TestInitialize]
         public void Setup()
@@ -46,6 +46,12 @@ namespace fa.Tests
             Assert.IsTrue(fa1.Run("1101") == true);
         }
         
+        [TestMethod]
+        public void FA1_Accept_11111101()
+        {
+            Assert.IsTrue(fa1.Run("11111101") == true);
+        }
+        
         // Отрицательные тесты для FA1 (должны отвергаться)
         [TestMethod]
         public void FA1_Reject_NoZero()
@@ -82,7 +88,7 @@ namespace fa.Tests
     [TestClass]
     public class FA2Tests
     {
-        private FA2 fa2;
+        private FA2 fa2 = null!;
         
         [TestInitialize]
         public void Setup()
@@ -90,71 +96,83 @@ namespace fa.Tests
             fa2 = new FA2();
         }
         
-        // Положительные тесты для FA2
+        // Положительные тесты для FA2 (нечетное кол-во 0 и нечетное кол-во 1)
         [TestMethod]
         public void FA2_Accept_01()
         {
+            // 0:1 шт (нечет), 1:1 шт (нечет)
             Assert.IsTrue(fa2.Run("01") == true);
         }
         
         [TestMethod]
         public void FA2_Accept_10()
         {
+            // 0:1 шт (нечет), 1:1 шт (нечет)
             Assert.IsTrue(fa2.Run("10") == true);
-        }
-        
-        [TestMethod]
-        public void FA2_Accept_0011()
-        {
-            Assert.IsTrue(fa2.Run("0011") == true);
-        }
-        
-        [TestMethod]
-        public void FA2_Accept_1100()
-        {
-            Assert.IsTrue(fa2.Run("1100") == true);
-        }
-        
-        [TestMethod]
-        public void FA2_Accept_0101()
-        {
-            Assert.IsTrue(fa2.Run("0101") == true);
-        }
-        
-        [TestMethod]
-        public void FA2_Accept_1010()
-        {
-            Assert.IsTrue(fa2.Run("1010") == true);
         }
         
         [TestMethod]
         public void FA2_Accept_000111()
         {
+            // 0:3 шт (нечет), 1:3 шт (нечет)
             Assert.IsTrue(fa2.Run("000111") == true);
+        }
+        
+        [TestMethod]
+        public void FA2_Accept_111000()
+        {
+            // 0:3 шт (нечет), 1:3 шт (нечет)
+            Assert.IsTrue(fa2.Run("111000") == true);
+        }
+        
+        [TestMethod]
+        public void FA2_Accept_010101()
+        {
+            // 0:3 шт (нечет), 1:3 шт (нечет)
+            Assert.IsTrue(fa2.Run("010101") == true);
+        }
+        
+        [TestMethod]
+        public void FA2_Accept_101010()
+        {
+            // 0:3 шт (нечет), 1:3 шт (нечет)
+            Assert.IsTrue(fa2.Run("101010") == true);
+        }
+        
+        [TestMethod]
+        public void FA2_Accept_0()
+        {
+            // Только для проверки - должно быть false, так как нет нечетного количества 1
+            Assert.IsFalse(fa2.Run("0") == true);
         }
         
         // Отрицательные тесты для FA2
         [TestMethod]
         public void FA2_Reject_EvenZerosEvenOnes()
         {
+            // 0:2 шт (чет), 1:2 шт (чет)
             Assert.IsFalse(fa2.Run("0011") == true);
-            Assert.IsFalse(fa2.Run("0011") == true);
+            Assert.IsFalse(fa2.Run("1100") == true);
+            Assert.IsFalse(fa2.Run("0101") == true);
+            Assert.IsFalse(fa2.Run("1010") == true);
         }
         
         [TestMethod]
         public void FA2_Reject_OddZerosEvenOnes()
         {
-            Assert.IsFalse(fa2.Run("0") == true);
-            Assert.IsFalse(fa2.Run("001") == true);
-            Assert.IsFalse(fa2.Run("0001") == true);
+            // 0:1 шт (нечет), 1:2 шт (чет)
+            Assert.IsFalse(fa2.Run("011") == true);
+            // 0:3 шт (нечет), 1:2 шт (чет)
+            Assert.IsFalse(fa2.Run("00011") == true);
         }
         
         [TestMethod]
         public void FA2_Reject_EvenZerosOddOnes()
         {
-            Assert.IsFalse(fa2.Run("1") == true);
-            Assert.IsFalse(fa2.Run("011") == true);
-            Assert.IsFalse(fa2.Run("0111") == true);
+            // 0:2 шт (чет), 1:1 шт (нечет)
+            Assert.IsFalse(fa2.Run("001") == true);
+            // 0:2 шт (чет), 1:3 шт (нечет)
+            Assert.IsFalse(fa2.Run("00111") == true);
         }
         
         [TestMethod]
@@ -167,7 +185,7 @@ namespace fa.Tests
     [TestClass]
     public class FA3Tests
     {
-        private FA3 fa3;
+        private FA3 fa3 = null!;
         
         [TestInitialize]
         public void Setup()
@@ -175,7 +193,7 @@ namespace fa.Tests
             fa3 = new FA3();
         }
         
-        // Положительные тесты для FA3
+        // Положительные тесты для FA3 (содержат "11")
         [TestMethod]
         public void FA3_Accept_11()
         {
@@ -218,7 +236,7 @@ namespace fa.Tests
             Assert.IsTrue(fa3.Run("10110") == true);
         }
         
-        // Отрицательные тесты для FA3
+        // Отрицательные тесты для FA3 (не содержат "11")
         [TestMethod]
         public void FA3_Reject_No11()
         {
@@ -228,6 +246,7 @@ namespace fa.Tests
             Assert.IsFalse(fa3.Run("01") == true);
             Assert.IsFalse(fa3.Run("101") == true);
             Assert.IsFalse(fa3.Run("010") == true);
+            Assert.IsFalse(fa3.Run("10101") == true);
         }
         
         [TestMethod]
